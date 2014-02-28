3842cde5728c41f98a86bb95462d4a73632343b4
Jan Baer <jan@janbaer.de>
2014-02-28 07:59:39 +0100

    Implemented app.js and directoryReader

--- app.js
+++ app.js
@@ -2,19 +2,41 @@
 
 'use strict';
 
-var fs = require('fs');
-//var path       = require('path');
+var fs = require('fs'),
+    program = require('commander'),
+    Q = require('q');
 
-var program = require('commander');
+var directoryReader = require('./lib/directoryReader.js');
 
 program.version('0.0.1')
        .option('-d, --directory', 'name of the directory')
        .option('-wi, --what-if', 'simulates the operations only')
        .parse(process.argv);
 
-if (program.directory === undefined) {
-  console.log('You have to provide the name of the directory in the -d argument!');
-  return;
+var directories = [];
+if (program.directory !== undefined) {
+  directories.push(program.directory);
+} else {
+  directories = require('./config/config.dev.json');
 }
+
+var promises = [];
+
+directories.forEach(function (directory) {
+  var promise = directoryReader.init(directory).then(directoryReader.read(directory));
+  promises.push(promise);
+});
+
+Q.all(promises)
+  .done(function () {
+    console.log('All directories successful processed...');
+  }, function (error) {
+    console.error('Error while processing the defined directories: %s', error);
+  });
+
+
+
+
+
 
 
