cd58ca24e0ef7bcb545e38923873e4f28cf7efd0
Jan Baer <jan@janbaer.de>
2014-03-03 20:04:11 +0100

    Promise handling improved

--- app.js
+++ app.js
@@ -45,6 +45,24 @@ var filter = function(pattern, callback) {
   };
 };
 
+var processDirectories = function (directories, callback) {
+  var promises = [];
+
+  directories.forEach(function (directory) {
+    promises.push(callback(directory));
+  });
+
+  return Q.all(promises);
+};
+
+var initAllDirectories = function () {
+  return processDirectories(directories, directoryReader.init);
+};
+
+var checkAllDirectories = function () {
+  return processDirectories(directories, directoryReader.readDir);
+};
+
 var watchDirectory = function (directoryPath, whatIf) {
   log.info(util.format('Watching "%s"', directoryPath));
   watch(directoryPath, { recursive: false }, filter(/\.mp3$/, function(filename) {
@@ -57,22 +75,6 @@ var watchDirectory = function (directoryPath, whatIf) {
   }));
 };
 
-var initAllDirectories = function () {
-  var promises = [];
-  directories.forEach(function (directory) {
-    promises.push(directoryReader.init(directory));
-  });
-  return Q.all(promises);
-};
-
-var checkAllDirectories = function () {
-  var promises = [];
-  directories.forEach(function (directory) {
-    promises.push(directoryReader.readDir(directory));
-  });
-  return Q.all(promises);
-};
-
 var watchAllDirectories = function () {
   log.info('All directories successful processed...');
   log.info('Now I am going into the watch mode');
@@ -81,19 +83,10 @@ var watchAllDirectories = function () {
   });
 };
 
-initAllDirectories()
+Q.fcall(initAllDirectories)
   .then(checkAllDirectories)
   .then(watchAllDirectories)
   .catch(function (error) {
     log.error(util.format('Error while processing the defined directories: %s', error));
   })
   .done();
-
-
-
-
-
-
-
-
-
