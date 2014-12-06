d4fe0b5b0e7044b28841096413557e6235218a6d
Jan Baer <jan@janbaer.de>
2014-12-06 14:44:59 +0100

    Changed directoryReader to work in a synchronous way

--- test/directoryReaderSpec.js
+++ test/directoryReaderSpec.js
@@ -11,86 +11,86 @@ var directoryReader = require('../lib/directoryReader.js');
 var fileChecker = require('../lib/fileChecker.js');
 
 describe('directoryReader Spec', function () {
-  describe('When Directory contains some mp3 files', function () {
-    var sandbox;
+  var sandbox;
+  var readDirCallbackSpy;
+
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+    readDirCallbackSpy = sinon.spy();
+  });
+
+  describe('readDir', function () {
+    beforeEach(function () {
+      sandbox.stub(fs, 'existsSync').returns(true);
+      sandbox.stub(fs, 'readdirSync').returns(['file1.mp3', 'file2.mp3', 'file3.mp3']);
+    });
 
     beforeEach(function () {
-      sandbox = sinon.sandbox.create();
+      directoryReader.readDir('~/download/mp3', readDirCallbackSpy);
     });
 
-    describe('readDir', function () {
+    it('Should call the callbackfunction for each file', function () {
+      expect(readDirCallbackSpy.callCount).to.equal(3);
+    });
+  });
+
+  describe('and some files are not mp3 files', function () {
+    beforeEach(function () {
+      sandbox.stub(fs, 'existsSync').returns(true);
+      sandbox.stub(fs, 'readdirSync').returns(['file1.mp3', 'file2.txt', 'file3.mp3']);
+    });
+
+    beforeEach(function () {
+      directoryReader.readDir('~/Downloads/mp3', readDirCallbackSpy);
+    });
+
+    it('Should just call the callback just with mp3 files', function () {
+      expect(readDirCallbackSpy.callCount).to.equal(2);
+    });
+  });
+
+  describe('Init', function () {
+    var testDirectoryPath = '~/Downloads/mp3',
+        processedDirectoryPath = '~/Downloads/mp3/processed';
+
+    describe('When the processed directory not exists', function () {
       beforeEach(function () {
-        sandbox.stub(fileChecker, 'check').returns(q.resolve(true));
+        var fsExistsStub = sandbox.stub(fs, 'existsSync');
+        fsExistsStub.withArgs(testDirectoryPath).returns(true);
+        fsExistsStub.withArgs(processedDirectoryPath).returns(false);
+        sandbox.stub(fs, 'mkdirSync');
       });
 
-      it('Should call the fileChecker for each file', function (done) {
-        sandbox.stub(fs, 'exists').yields(true);
-        sandbox.stub(fs, 'readdir').yields(null, ['file1.mp3', 'file2.mp3', 'file3.mp3']);
-
-        directoryReader.readDir('~/download/mp3')
-          .done(function () {
-            expect(fileChecker.check.callCount).to.equal(3);
-            done();
-          });
+      beforeEach(function () {
+        directoryReader.init(testDirectoryPath);
       });
 
-      describe('and some files are not mp3 files', function () {
-        beforeEach(function () {
-          sandbox.stub(fs, 'readdir').yields(null, ['file1.txt']);
-        });
+      it('Should check if the directories exists', function () {
+        expect(fs.existsSync.callCount).to.equal(2);
+      });
 
-        it('Should just call the files with mp3 files', function (done) {
-          directoryReader.readDir('~/Downloads/mp3')
-            .done(function () {
-              expect(fileChecker.check.notCalled).to.be.true;
-              done();
-            });
-        });
+      it('Should create the processed directory', function () {
+        expect(fs.mkdirSync.calledWith(processedDirectoryPath)).to.be.true;
       });
     });
 
-    describe('Init', function () {
-      var testDirectoryPath = '~/Downloads/mp3',
-          processedDirectoryPath = '~/Downloads/mp3/processed';
-
-      describe('When the processed directory not exists', function () {
-
-        beforeEach(function () {
-          var fsExistsStub = sandbox.stub(fs, 'exists');
-          fsExistsStub.withArgs(testDirectoryPath).yields(true);
-          fsExistsStub.withArgs(processedDirectoryPath).yields(false);
-          sandbox.stub(fs, 'mkdir').yields(null);
-        });
-
-        it('Should create it', function (done) {
-          directoryReader.init(testDirectoryPath)
-            .done(function () {
-              expect(fs.exists.called).to.be.true;
-              expect(fs.mkdir.calledWith(processedDirectoryPath)).to.be.true;
-              done();
-            });
-        });
+    describe('When the processed directory exists', function () {
+      beforeEach(function () {
+        sandbox.stub(fs, 'existsSync').returns(true);
+        sandbox.stub(fs, 'mkdirSync');
       });
 
-      describe('When the processed directory exists', function () {
-        beforeEach(function () {
-          sandbox.stub(fs, 'exists').yields(true);
-          sandbox.stub(fs, 'mkdir').yields(null);
-        });
+      beforeEach(function () {
+        directoryReader.init(testDirectoryPath);
+      });
 
-        it('Should not try to create directory', function (done) {
-          directoryReader.init(testDirectoryPath)
-            .done(function () {
-              expect(fs.exists.called).to.be.true;
-              expect(fs.mkdir.called).to.be.false;
-              done();
-            });
-        });
+      it('Should not try to create directory', function () {
+        expect(fs.mkdirSync.called).to.be.false;
       });
     });
+  });
 
-    afterEach(function () {
-      sandbox.restore();
-    });
+  afterEach(function () {
+    sandbox.restore();
   });
 });
