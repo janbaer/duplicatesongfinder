cc83952d04d2d5f329383eb714d9b37b9813bd55
Jan Baer <jan@janbaer.de>
2014-12-13 17:56:44 +0100

    Add process.exit

--- export.js
+++ export.js
@@ -3,12 +3,17 @@
 var fs = require('fs');
 var path = require('path');
 var redis = require('./lib/redisdb.js');
+var util = require('util');
 
 redis.getAllKeys().then(function (keys) {
+  console.log(util.format('Begin exporting %d keys', keys.length));
+
   var fileName = path.resolve(path.join('./config', 'mp3keys.json'));
 
   fs.writeFile(fileName, JSON.stringify(keys), function () {
     console.log('All keys exported to ', fileName);
+
+    process.exit();
   });
 });
 
